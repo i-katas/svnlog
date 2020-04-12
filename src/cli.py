471dc6284f7d23137d4c499ee10c989ec09ffddf
i-katas <feexonj@gmail.com>
2020-04-13 01:13:07 +0800

    fail fast if stdin is tty

--- src/cli.py
+++ src/cli.py
@@ -8,37 +8,39 @@ STDIN = str(b'/dev/stdin')
 
 def main(*args, stdin: Union[str, IO] = sys.stdin, write: Callable[[str], None] = print):
     try:
-        parser = build_parser(stdin)
+        args = args or sys.argv[1:]
+        parser = build_parser(redirect(stdin))
+        if (not args or STDIN in args) and hasattr(stdin, 'isatty') and stdin.isatty():
+            raise SystemExit(parser.format_help())
 
-        options = parser.parse_args(args or sys.argv[1:])
+        options = parser.parse_args(args)
 
-        write(svnlog.format(svnlog.parse(options.file, remote_path=options.remote_path), template=load_template(options.template)))
+        write(svnlog.format(svnlog.parse(options.file, options.remote_path), text_of(options.template, svnlog._DEFAULT_TEMPLATE_)))
     except FileNotFoundError as e:
         raise SystemExit(f'{e.strerror}: {e.filename}')
-    except IOError:
-        raise SystemExit(parser.format_help())
 
 
-def build_parser(stdin):
-    def vfile(path):
+def redirect(stdin):
+    def wrap(path):
         if path is STDIN:
             if isinstance(stdin, str):
                 return open(stdin)
-            elif stdin.isatty():
-                raise IOError("Unsupported tty")
             else:
                 return stdin
         return open(path)
+    return wrap
 
+
+def build_parser(source):
     parser = ArgumentParser()
-    parser.add_argument('file', type=vfile, nargs='?', default=STDIN, help='the svn xml format log file')
-    parser.add_argument('-t', '--template', type=open, help='svn log template file')
-    parser.add_argument('-p', '--remote-path', type=str, help='remote path relative to current working directory')
+    parser.add_argument('file', type=source, nargs='?', default=STDIN, help='the svn xml format log file')
+    parser.add_argument('-t', '--template', type=open, help='use custom log template file')
+    parser.add_argument('-p', '--remote-path', type=str, help='remove the remote path from the path of log entry')
     return parser
 
 
-def load_template(tpl: Optional[IO]) -> str:
-    return ''.join(tpl.readlines()) if tpl else svnlog._DEFAULT_TEMPLATE_
+def text_of(source: Optional[IO], default: str = None) -> str:
+    return ''.join(source.readlines()) if source else default
 
 
 if __name__ == '__main__':
