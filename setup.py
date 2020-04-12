2ddf98d46ddafe42ca487b1839326e0d3aceb754
i-katas <feexonj@gmail.com>
2020-04-13 02:01:37 +0800

    add svnlog metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     package_dir={'': 'src'},
     packages=[''],
+    python_requires='>=3.6',
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     extras_require=dict(
